3e8d7e85fbe90d1ba984d688f29fca9c31c723b5
js6pak <kubastaron@hotmail.com>
2022-03-10 19:05:43 +0100

    Workaround FastStringService issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,39 +1,43 @@
 import groovy.json.JsonSlurper
+import groovy.json.JsonOutput
 import java.net.URL
 
 @Suppress("UNCHECKED_CAST")
 task("build") {
-    buildDir.mkdir()
+    doLast {
+        buildDir.mkdir()
 
-    val index = JsonSlurper().parse(URL("https://betacraft.uk/server-archive/server_index.json")) as List<Map<String, *>>
+        val parser = JsonSlurper()
 
-    val manifest = JsonSlurper().parse(URL("https://launchermeta.mojang.com/mc/game/version_manifest_v2.json")) as Map<String, *>
+        val index = parser.parse(URL("https://betacraft.uk/server-archive/server_index.json")) as List<Map<String, *>>
+        val manifest = parser.parse(URL("https://launchermeta.mojang.com/mc/game/version_manifest_v2.json")) as Map<String, *>
 
-    (manifest["versions"] as List<MutableMap<String, Any>>).parallelStream().forEach { version ->
-        val id = version["id"] as String
-        val url = version["url"] as String
+        (manifest["versions"] as List<MutableMap<String, Any>>).parallelStream().forEach { version ->
+            val id = version["id"] as String
+            val url = version["url"] as String
 
-        val versionInfo = JsonSlurper().parse(URL(url)) as Map<String, *>
-        val downloads = versionInfo["downloads"] as MutableMap<String, Map<String, *>>
+            val versionInfo = parser.parse(URL(url)) as Map<String, *>
+            val downloads = versionInfo["downloads"] as MutableMap<String, Map<String, *>>
 
-        if (!downloads.containsKey("server")) {
-            val serverInfo = index.singleOrNull { (it["names"] as List<String>).contains(id) }
+            if (!downloads.containsKey("server")) {
+                val serverInfo = index.singleOrNull { (it["names"] as List<String>).contains(id) }
 
-            if (serverInfo != null) {
-                val formats = serverInfo["available_formats"] as List<Map<String, *>>
-                val jarFormat = formats.single { it["format"] as String == "jar" }
+                if (serverInfo != null) {
+                    val formats = serverInfo["available_formats"] as List<Map<String, *>>
+                    val jarFormat = formats.single { it["format"] as String == "jar" }
 
-                downloads["server"] = mapOf(
-                    "sha1" to (jarFormat["sha1"] as String).toLowerCase(),
-                    "size" to jarFormat["size"] as Int,
-                    "url" to jarFormat["url"] as String,
-                )
+                    downloads["server"] = mapOf(
+                        "sha1" to (jarFormat["sha1"] as String).toLowerCase(),
+                        "size" to jarFormat["size"] as Int,
+                        "url" to jarFormat["url"] as String
+                    )
 
-                version["url"] = "https://babric.github.io/manifest-polyfill/$id.json"
-                File(buildDir, "$id.json").writeText(groovy.json.JsonOutput.toJson(versionInfo))
+                    version["url"] = "https://babric.github.io/manifest-polyfill/$id.json"
+                    File(buildDir, "$id.json").writeText(JsonOutput.toJson(versionInfo))
+                }
             }
         }
-    }
 
-    File(buildDir, "version_manifest_v2.json").writeText(groovy.json.JsonOutput.toJson(manifest))
+        File(buildDir, "version_manifest_v2.json").writeText(JsonOutput.toJson(manifest))
+    }
 }
